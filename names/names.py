4977714066059ab76775fbcaa93960eb38f9e86a
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-01-21 19:04:51 +0530

    Update names.py

--- names/names.py
+++ names/names.py
@@ -1,12 +1,12 @@
 '''
 name = input("Enter the name: ")
-file = open("name.txt", "a")
+file = open("names.txt", "a")
 file.write(f"{name}\n")
 file.close()
-# here we have closed the file using the above function. This is a manual way
+# Here we have closed the file using the above function. This is a manual way
 '''
 
-# This is more pythonic way. Now we will use "with" that will indicate to automatically open the name.txt and also close it.
+# This is the more Pythonic way. Now we will use "with" that will indicate to automatically open the name.txt and also close it.
 name = input("Enter the name: ")
 
 with open("name.txt", "a") as file:
