45e345517313304d2ecf3b40253b1c47a4692c16
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-01-24 23:44:35 +0530

    Update students.py

--- students/students.py
+++ students/students.py
@@ -1,4 +1,4 @@
-# Here we are sorting out the lines in the file which is just english
+# Here we are sorting out the lines in the file which is just English
 '''
 with open("students.csv") as file:
     for line in sorted(file):
@@ -16,14 +16,18 @@ with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
         student = {"name": name, "house": house}        #dictionary with "name" as key and "house" as value
-        students.append(student)            #appending one student at a time as a dictionary in the students list
+        students.append(student)            #appending one student at a time as a dictionary in the students' list
 
+
+# Created solely for returning the value of the key name which is called by the sorted function to sort
 def get_name(student):
   return student['name']
 
+
+# Created solely for returning the value of the key house which is called by the sorted function to sort
 def get_house(student):
    return student['house']
 
-# Using above Python feature, we can pass get_name function as argument to below sorted function as key
-for student in sorted(students, key=get_house):
+# Using the above Python feature, we can pass get_name function as an argument to the below sorted function as the key
+for student in sorted(students, key=get_name):
   print(f"{student['name']} is in {student['house']}")
